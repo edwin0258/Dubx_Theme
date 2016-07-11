f51621b91991d377e5c4f99dd03e1f9694326896
Devin <edwin0258@gmail.com>
2016-07-11 16:45:58 -0700

    Add soundcloud link compatibility

--- TextToYoutubeLinks/nodeVersion.js
+++ TextToYoutubeLinks/nodeVersion.js
@@ -13,14 +13,15 @@ fs.readFile('stuff.txt','utf-8',function(err, data){
 	.filter(function(x,i){if(i % 2 == 1) return x})
 	.reduce(function(obj,x,i,arr){
 		if(i % 2 == 1)
-			obj.push({name: arr[i-1],link: 'https://www.youtube.com/watch?v=' + arr[i]})
-			
+			(/^\d+$/g.test(arr[i])) ?
+			obj.push({name: arr[i-1],type:'soundcloud',link: 'https://w.soundcloud.com/player/?url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F' + arr[i]}) :
+			obj.push({name: arr[i-1],type:'youtube',link: 'https://www.youtube.com/watch?v=' + arr[i]})
 		return obj
 	}
 	, [])
 
     
-    fs.writeFile('video_links.txt', db.map(x => 'Name: ' + x.name + ' Link: ' + x.link + '\n').join(''),'utf-8',function(err){
+    fs.writeFile('video_links.txt', db.map(x => 'Name: ' + x.name + '\n Link: ' + x.link + '\n').join(''),'utf-8',function(err){
         if(err){ console.log(err)
         return err}
     })
--- TextToYoutubeLinks/vanillaJavascriptVersion.js
+++ TextToYoutubeLinks/vanillaJavascriptVersion.js
@@ -7,13 +7,14 @@ db = db.split(/host_id">(\w{1,})<|name="name">([\w!@#$%^&*()_+\-=\[\]{};':"\\|,.
 	.filter(function(x,i){if(i % 2 == 1) return x})
 	.reduce(function(obj,x,i,arr){
 		if(i % 2 == 1)
-			obj.push({name: arr[i-1],link: 'https://www.youtube.com/watch?v=' + arr[i]})
-			
+			(/^\d+$/g.test(arr[i])) ?
+			obj.push({name: arr[i-1],type:'soundcloud',link: 'https://w.soundcloud.com/player/?url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F' + arr[i]}) :
+			obj.push({name: arr[i-1],type:'youtube',link: 'https://www.youtube.com/watch?v=' + arr[i]})
 		return obj
 	}
 	, [])
 
 console.log(db) // [object] form
 // comment out which version you don't want
-console.log(db.reduce(function(arr,x,i){arr.push([x.name,x.link])
+console.log(db.reduce(function(arr,x,i){arr.push([x.name,x.type,x.link])
 return arr},[])) // multi-dim array form
