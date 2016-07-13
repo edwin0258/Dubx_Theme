5852cdbcfb840dc6552a33b1b0ca406c9b432346
Devin <edwin0258@gmail.com>
2016-07-12 19:57:56 -0700

    Fix regexp

--- TextToYoutubeLinks/vanillaJavascriptVersion.js
+++ TextToYoutubeLinks/vanillaJavascriptVersion.js
@@ -3,7 +3,7 @@
 var db = 'Your raw html here, e.g. "<column name="id">4</column> ect.."'
 
 //replace name="name" with name="slug" if problems.
-db = db.split(/host_id">(\w{1,})<|name="name">([\w!@#$%^&*()_+\-=\[\]{};':"\\|,.\/? \u1100-\u11FF\u3130\u318F\uA960-\uA97F\uAC00-\uD7AF\uD7B0-\uD7FF\u3000-\u303F|\u3040-\u309F|\u30A0-\u30FF|\uFF00-\uFFEF|\u4E00-\u9FAF|\u2605-\u2606|\u2190-\u2195|\u203B/]{1,})</g)
+db = db.split(/host_id">(.*?)<|name="name">(.*?)</g)
 	.filter(function(x){ return x !== undefined})
 	.filter(function(x,i){if(i % 2 == 1) return x})
 	.reduce(function(obj,x,i,arr){
